3a4296ef779c4c35fb63c6f1ccc8880ac4dc0611
cheezi0747 <linus.sjolinder@b3.se>
2024-09-29 14:47:42 +0200

    : adding stuff

--- commit/commit.go
+++ commit/commit.go
@@ -31,9 +31,9 @@ func CommitChanges(repoPath, branchName, commitMessage string) error {
 	}
 
 	done <- true
-	//green := color.New(color.FgGreen).SprintFunc()
+	green := color.New(color.FgGreen).SprintFunc()
 	statusMessage = fmt.Sprintf("%s: committed and pushed to remote", repoName)
-	fmt.Printf("\r%s", statusMessage)
+	fmt.Printf("\r%s [%s] ---", statusMessage, green("✔"))
 	return nil
 }
 
